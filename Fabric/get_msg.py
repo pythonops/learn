db01861f09a10daf9d11650f0759aeeff2113104
Cheny <124236270@qq.com>
2017-07-26 16:25:18 +0800

    get_msg.py

--- Fabric/get_msg.py
+++ Fabric/get_msg.py
@@ -1,10 +1,15 @@
+# -*- encoding:utf-8 -*-
+#加载Fabric API模块
 from fabric.api import *
+#加载execute方法，用于获取task返回值。
 from fabric.tasks import execute
+#加载hide方法，用于隐藏Fabric各级别输出信息。
 from fabric.context_managers import hide
 
 web_servers = ["ops@web-1","ops@web-2"]
 db_servers = ["ops@db-1","ops@db-2"]
 
+#定义角色
 env.roledefs = {
     "web" : web_servers,
     "db" : db_servers,
@@ -13,20 +18,22 @@ env.roledefs = {
 @roles("web")
 @task
 def get_ip():
-    shell_command = ''' ifconfig |grep 'inet addr'|awk -F ":" '{print $2}'|awk '{print $1}' '''
+    shell_command = ''' ifconfig ens33 |grep 'inet addr'|awk -F ":" '{print $2}'|awk '{print $1}' '''
     return run(shell_command)
 
 @roles("db")
 @task
 def get_disk():
-    return run("df -h /")
+    shell_command = ''' df -h / | awk '{print $4}' | grep '^[0-9].*' '''
+    return run(shell_command)
 
 @task
 def go():
+    #隐藏Fabric输出信息。
     with hide('running', 'stdout', 'stderr'):
         result_web =  execute(get_ip)
         result_db = execute(get_disk)
         return result_web,result_db
     
 if __name__ == '__main__':
-    print go()
+    print go() 
